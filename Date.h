fee5fb8be9410862709d9852eceec849b77b0e76
RockADai <15968190152@163.com>
2019-05-09 22:11:48 +0800

    Practicing C++

---
+++ Date.h
@@ -0,0 +1,23 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Date
+{
+private:
+    unsigned year;
+    unsigned month;
+    unsigned day;
+public:
+    Date();
+    Date(string date_str);
+    ~Date();
+    void show();
+};
+
+#endif
