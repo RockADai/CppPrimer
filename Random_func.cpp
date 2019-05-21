7d2423a3f4929c31ad3a8ba2fcad277a60af13cd
RockADai <15968190152@163.com>
2019-05-21 19:35:28 +0800

    Practicing C++

---
+++ Random_func.cpp
@@ -0,0 +1,24 @@
+#include <random>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void get_random(){
+    default_random_engine e;
+    normal_distribution<> n(4,1.5);
+    vector<unsigned> vals(9);
+    for(size_t i = 0;i != 200;++i){
+        unsigned v = lround(n(e));
+        if(v < vals.size())
+            ++vals[v];
+    }
+    for(size_t j = 0;j != vals.size();++j)
+        cout << j << ": " << string(vals[j],'*') << endl;
+}
+
+int main(){
+    get_random();
+    return 0;
+}
